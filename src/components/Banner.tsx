617d330bec2125fa8738a8f07f4821a4e14384e0
Jose Victor <59773190+josevictoremiliano@users.noreply.github.com>
2024-03-27 19:56:58 -0300

    Update Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -10,9 +10,9 @@ export default function Banner() {
             
             <div className="max-w-screen-xl mx-auto flex flex-col items-center relative">
                 <div className="mt-[10em] mx-6">
-                    <div className="text-white text-2xl md:text-4xl font-semibold">Olá! eu sou </div>
-                    <div className="linearGradientText text-5xl md:text-6xl font-bold ">Jose Victor</div>
-                    <div className="max-w-2xl text-white  text-lg md:text-xl font-bold">Web design, Design gráfico,  Tecnico em  Informatica para Internet pelo IFRN/Brasil, residindo no Rio Grande do Norte- Brasil.</div>
+                    <div className="text-white text-2xl md:text-4xl font-semibold">Olá! Eu sou </div>
+                    <div className="linearGradientText text-5xl md:text-6xl font-bold ">José Victor</div>
+                    <div className="max-w-2xl text-white  text-lg md:text-xl font-bold">Web design, design gráfico, técnico em Informática para Internet pelo IFRN/Brasil, residindo no Rio Grande do Norte-Brasil.</div>
 
                     <div className="flex mt-8 btn-banner">
                         <Link href="#About" className="bg-acerola text-white px-8 py-3 rounded-full font-bold mr-4 hover:bg-orange-700 transition-all">Me conheça</Link>
