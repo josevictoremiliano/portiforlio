91bcf4a542383a8a69b6818c8fb9ed953fae507a
Jose Victor <59773190+josevictoremiliano@users.noreply.github.com>
2024-03-27 20:02:54 -0300

    Update projetos.tsx

--- src/components/projetos.tsx
+++ src/components/projetos.tsx
@@ -9,19 +9,26 @@ import ImagemNeep from "public/projetos/neep.png"
 
 
 const projects = [
+     {
+        title: "Redesign do SigEduc",
+        tags: ["Figma", "HTML", "CSS", "JavaScript"],
+        image: "/projetos/sigeduc.png",
+        description: "Redesign do SigEduc para se adequar aos padrões visuais da Web atuais e possibilitando melhorias de UX/UI.",
+        link: "https://josevictoremiliano.github.io/SigEduc/index.html"
+    },
     {
         title: "Achei",
         image: ImageAchei,
         tags: ["PHP", "Laravel", "Bootstrap", "Figma", "JavaScript", "jQuery", "HTML", "CSS"],
         description: "Compartilhamos informações sobre comércios, promoções e novidades.",
-        link: "https://achei.tech"
+        link: "hidden"
     },
     {
         title: "SEIA",
         image: ImagemSeia,
         tags: ["PHP", "Laravel", "Bootstrap", "Figma", "JavaScript", "jQuery", "HTML", "CSS"],
         description: "O SEIA Escolar é um sistema de gestão escolar que ajuda gestores, professores e alunos a gerenciar informações de alunos, matrículas, turmas, notas, frequência e outras funcionalidades essenciais para o dia a dia escolar.",
-        link: "https://seiaescolar.com"
+        link: "hidden"
     },
     {
         title: "Conexão cidadã",
@@ -36,13 +43,6 @@ const projects = [
         image:ImagemLux,
         description: "Lux é uma rede social pensada de forma unica e singular para cada usuário, onde você pode compartilhar seus momentos, fotos, vídeos, textos e muito mais. De forma simples e intuitiva.",
         linkDisponivel: "hidden",
-    },
-    {
-        title: "Redesign do SigEduc",
-        tags: ["Figma", "HTML", "CSS", "JavaScript"],
-        image: "/projetos/sigeduc.png",
-        description: "Redesign do SigEduc para se adequar aos padrões visuais da Web atuais e possibilitando melhorias de UX/UI.",
-        link: "https://josevictoremiliano.github.io/SigEduc/index.html"
     },
     {
         title: "Site do PEPP - IFRN/SPP",
